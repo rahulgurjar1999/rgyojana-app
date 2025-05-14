b9c399c3e097d4abc634880fcf215652d29da853
rahulgurjar1999 <rahulgurjaralone@gmail.com>
2025-05-14 12:15:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>RG Yojana Connect</h1>
+      <p>Your Yojana, Your Doorstep</p>
+      <p>भारत की सभी सरकारी योजनाएं, अब आपके द्वार</p>
+    </div>
+  );
+}
+
+export default App;
